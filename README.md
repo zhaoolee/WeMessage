124b0d81af46eb3b82032fc42115554f04a12c3e
zhaoolee <zhaoolee@gmail.com>
2021-06-02 23:30:50 +0800

    ok

--- README.md
+++ README.md
@@ -15,3 +15,20 @@ nvm use v14.16.0
 - 客户端使用小程序框架taro构建 @tarojs/cli@3.2.10
 - 服务端使用稳健的Node.js框架Express
 
+## 新建数据库，数据表
+
+
+```sql
+--创建数据库
+CREATE DATABASE IF NOT EXISTS WEMESSAGE DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+--创建留言数据表
+CREATE TABLE IF NOT EXISTS `message`(
+   `snow` VARCHAR(100) NOT NULL comment '雪花字段',
+   `markdown_file_name` VARCHAR(200) NOT NULL comment '与文章内容绑定的markdown文章名也可以放阅读原文的url',
+   `nick_name` VARCHAR(100) default "" comment '用户昵称',
+   `message` VARCHAR(1000) default "" comment '留言信息',
+   `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP comment '创建时间',
+    `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '更新时间',
+   PRIMARY KEY ( `snow` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+```
--- server/source/wemessage-index.js
+++ server/source/wemessage-index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-var cors = require('cors')
+const cors = require('cors')
 const app = express()
 const port = 3600
+const mysql = require('mysql');
+const db_conf = require('./db_conf.js');
 
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(cors())
+
 
 
 
@@ -25,6 +34,23 @@ app.post('/wemessage/onLogin', (req, res) => {
         name: "zhaoolee",
         age: 25
     })
+})
+
+app.post('/wemessage/message', (req, res)=>{
+    const { markdown_file_name, nick_name, message } = req.body
+
+    
+
+    var connection = mysql.createConnection(db_conf.db.online);
+       
+      connection.connect();
+       
+      connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+      });
+       
+      connection.end();
 })
 
 
@@ -59,4 +85,5 @@ app.get('/wemessage/message', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
+    console.log("==>>", db_conf.db.online);
 })
